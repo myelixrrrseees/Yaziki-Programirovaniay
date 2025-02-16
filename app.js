1597d3be711c913a05366ee75a2534745d691076
myelixrrrseees <myelixrrrseees@gmail.com>
2025-02-16 16:47:30 +0700

    First commit

---
+++ app.js
@@ -0,0 +1,36 @@
+/*1. Написать программу, которая содержит не менее 5-и переменных (для этого
+используйте ключевое слов let). Присвоить им значения разных типов и с помощью
+функции typeof() вывести тип каждой переменной на экран.*/
+
+let name = "Иван";
+let age = 25;
+let isStudent = true;
+let height = null;
+let city;
+
+console.log("Тип переменной name:", typeof name);
+console.log("Тип переменной age:", typeof age);
+console.log("Тип переменной isStudent:", typeof isStudent);
+console.log("Тип переменной height:", typeof height);
+console.log("Тип переменной city:", typeof city);
+
+console.log("")
+
+/*2. Создайте и присвойте значения двум переменным (для этого используйте ключевое
+слов let). С помощью операторов сравнения проверить, удовлетворяет ли первая
+переменная следующим условиям:
+o равна второй;
+o меньше второй;
+o меньше или равна второй;
+o больше второй.*/
+
+let a = 10;
+let b = 20;
+
+console.log("a == b:", a == b);
+console.log("a < b:", a < b);
+console.log("a <= b:", a <= b);
+console.log("a > b:", a > b);
+
+console.log("")
+
